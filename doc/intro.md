a6cbd499abfb1a66ab76c3d87c05c21aa6a85987
Aki Kaivola <aki.kaivola@gmail.com>
2015-10-31 12:48:34 +0200

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to obd
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/obd/core_test.clj
@@ -0,0 +1,7 @@
+(ns obd.core-test
+  (:require [clojure.test :refer :all]
+            [obd.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
