1ad5a1133b818511db1656cf39e9871fb98f2989
Aki Kaivola <aki.kaivola@gmail.com>
2015-11-01 16:42:56 +0200

    Set up a working netcat read-write pipe

--- src/obd/tcp.clj
+++ src/obd/tcp.clj
@@ -10,12 +10,25 @@
 
 (def at-protocol
   (gloss/compile-frame
-   (gloss/string :utf-8 :delimiters ["\r" "\r\n"])
+   (gloss/string :utf-8 :delimiters ["\n"])
    identity
    identity))
+
+(defn wrap-duplex-stream
+  [protocol s]
+  (let [out (s/stream)]
+    (s/connect
+     (s/map #(io/encode at-protocol %) out)
+     s)
+    (s/splice
+     out
+     (io/decode-stream s at-protocol))))
+
 
 (defn client
   ([] (client "192.168.0.10" 35000))
   ([host port]
-    (d/chain (tcp/client {:host host, :port port}))))
+   (d/chain (tcp/client {:host host, :port port})
+            #(wrap-duplex-stream at-protocol %))))
 
+(def c (client "127.0.0.1" 1234))
