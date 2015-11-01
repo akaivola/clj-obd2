771a09d0e214520dfefaf42702ab2f2a1b7cb363
Aki Kaivola <aki.kaivola@gmail.com>
2015-11-01 16:23:44 +0200

    choose tech stack

--- project.clj
+++ project.clj
@@ -1,10 +1,17 @@
 (defproject obd "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description ""
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-serial "2.0.4-SNAPSHOT"]]
+                 [org.clojure/core.async "0.2.371"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [clj-serial "2.0.4-SNAPSHOT"]
+                 [aleph "0.4.1-beta1"]
+                 [manifold "0.1.1"] ; transitive of aleph
+                 [gloss "0.2.5"]    ; byte protocol abstraction library
+                 [byte-streams "0.2.0"]
+                ]
   :main ^:skip-aot obd.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
